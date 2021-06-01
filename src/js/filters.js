9d42d69f678727385d38f7245d25732443ee0088
igor-mircic <igor.mircic@live.com>
2021-06-01 12:58:01 +0200

    feat(filters): add beer name box

--- src/js/filters.js
+++ src/js/filters.js
@@ -5,3 +5,14 @@ let context = {
 };
 
 document.querySelector(".filters").innerHTML += filters(context);
+
+// document.getEelemntById("filters").html(Templates["templateFilters"](filters));
+// this.addFiltersListener();
+// addFiltersListener= () => {
+//         const buttons = Array.from(document.querySelectorAll('.view__filters .close'));
+//         buttons.forEach(button = >{
+//             button.addEventListener('click', (e) => {
+//                 toggleFilter(e.target);
+//             });
+//         });
+//     }
