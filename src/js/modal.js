944521ee577445435e5c5d649ece8f43465323a4
igor-mircic <igor.mircic@live.com>
2021-06-06 21:00:38 +0200

    feat(modal): wip

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,10 +1,7 @@
 import modalTemplate from "../templates/modal.handlebars";
 const { default: MicroModal } = require("micromodal");
 
-const displayModal = () => {
-  document.querySelector(".modal__container").innerHTML += modalTemplate();
-};
-
-document.querySelector("#toggle-modal").addEventListener("click", () => {
+export const renderModal = (data) => {
+  document.querySelector(".modal__container").innerHTML = modalTemplate(data);
   MicroModal.show("modal-1");
-});
+};
--- src/js/products.js
+++ src/js/products.js
@@ -1,4 +1,5 @@
 import products from "../templates/products.handlebars";
+import { renderModal } from "./modal";
 import { getBeerList } from "./services";
 
 const displayBeer = (beerList) => {
@@ -9,13 +10,23 @@ getBeerList().then((data) => {
   let beerList = [];
   for (let i = 0; i < data.length; i++) {
     beerList[i] = {
+      PID: "PID-" + data[i].id,
       name: data[i].name,
       description: data[i].description,
       img: data[i].image_url,
       price: "$" + data[i].abv * 10 + ".99",
     };
   }
-  console.log(data[0]);
-  console.log(data[0].description);
+  // console.log(data[0]);
   displayBeer(beerList);
+
+  const hoverBtns = document.querySelectorAll(".product__hover");
+
+  hoverBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const PID = btn.parentNode.parentNode.id.split("-")[1];
+      let beer = data.find((beer) => beer.id == PID);
+      renderModal(beer);
+    });
+  });
 });
