1f1c04321ed324fe66f398df20324aff73651e97
igor-mircic <igor.mircic@live.com>
2021-06-06 17:51:23 +0200

    feat(modal): wip

--- src/js/products.js
+++ src/js/products.js
@@ -9,9 +9,13 @@ getBeerList().then((data) => {
   let beerList = [];
   for (let i = 0; i < data.length; i++) {
     beerList[i] = {
+      name: data[i].name,
+      description: data[i].description,
       img: data[i].image_url,
       price: "$" + data[i].abv * 10 + ".99",
     };
   }
+  console.log(data[0]);
+  console.log(data[0].description);
   displayBeer(beerList);
 });
