88144ae968ce9cf2696c11398bfe0a6544d76d64
igor-mircic <igor.mircic@live.com>
2021-06-06 17:40:38 +0200

    feat(modal): wip

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,4 +1,9 @@
+import modalTemplate from "../templates/modal.handlebars";
 const { default: MicroModal } = require("micromodal");
+
+const displayModal = () => {
+  document.querySelector(".modal__container").innerHTML += modalTemplate();
+};
 
 document.querySelector("#toggle-modal").addEventListener("click", () => {
   MicroModal.show("modal-1");
