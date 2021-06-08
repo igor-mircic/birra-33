ca5baedda24e35617e1df770c29babd4997a6d4d
igor-mircic <igor.mircic@live.com>
2021-06-08 09:37:25 +0200

    feat(modal): add better logic for eventListeners

--- src/js/products.js
+++ src/js/products.js
@@ -2,30 +2,22 @@ import productTemplate from "../templates/products.handlebars";
 import { renderModal } from "./modal";
 import { getBeerList } from "./services";
 
-const productsWraper = document.querySelector(".products__wraper");
 getBeerList().then((data) => {
-  let beerList = [];
-  let beer = {};
-  for (let i = 0; i < data.length; i++) {
-    beer = {
-      id: data[i].id,
-      name: data[i].name,
-      description: data[i].description,
-      img: data[i].image_url,
-      price: "$" + data[i].abv * 10 + ".99",
-      ingredients: data[i].ingredients,
-      food_pairing: data[i].food_pairing,
-    };
-    beerList[i] = beer;
+  const productsWraper = document.querySelector(".products__wraper");
 
-    const product = document.createElement("li");
-    product.classList.add("product");
-    product.addEventListener("click", (e) => {
+  data.forEach((product) => {
+    product.price = "$" + product.abv * 10 + ".99";
+
+    const productElement = document.createElement("li");
+    productElement.classList.add("product");
+
+    productElement.addEventListener("click", (e) => {
       if (e.target.className === "product__hover") {
-        renderModal(beer);
+        renderModal(product);
       }
     });
-    product.innerHTML = productTemplate(beer);
-    productsWraper.appendChild(product);
-  }
+
+    productElement.innerHTML = productTemplate(product);
+    productsWraper.appendChild(productElement);
+  });
 });
