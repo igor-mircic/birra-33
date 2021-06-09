68693b78e93bb38b6490bd7502b0b6bf06683143
igor-mircic <igor.mircic@live.com>
2021-06-10 00:53:12 +0200

    ref(shop): made code more readable

--- src/js/shop.js
+++ src/js/shop.js
@@ -3,47 +3,48 @@ import { renderModal } from "./modal";
 import { getBeerList } from "./services";
 import { filters } from "./filters";
 
-const render = (filters) => {
-  getBeerList(1, 9, filters).then((beerList) => {
-    // console.log(data);
-    const productsWraper = document.querySelector(".products__wraper");
+const render = (beerList) => {
+  // console.log(data);
+  const productsWraper = document.querySelector(".products__wraper");
 
-    while (productsWraper.firstChild) {
-      productsWraper.removeChild(productsWraper.firstChild);
-    }
+  while (productsWraper.firstChild) {
+    productsWraper.removeChild(productsWraper.firstChild);
+  }
 
-    beerList.forEach((beer) => {
-      beer.price = "$" + beer.abv * 10;
+  beerList.forEach((beer) => {
+    beer.price = "$" + beer.abv * 10;
 
-      const productElement = document.createElement("li");
-      productElement.classList.add("product");
+    const productElement = document.createElement("li");
+    productElement.classList.add("product");
 
-      productElement.addEventListener("click", (e) => {
-        if (e.target.className === "product__hover") {
-          renderModal(beer);
-        }
-      });
-
-      productElement.innerHTML = productTemplate(beer);
-      productsWraper.appendChild(productElement);
-      // console.log(product);
+    productElement.addEventListener("click", (e) => {
+      if (e.target.className === "product__hover") {
+        renderModal(beer);
+      }
     });
+
+    productElement.innerHTML = productTemplate(beer);
+    productsWraper.appendChild(productElement);
+    // console.log(product);
   });
 };
 
-filters.init();
+const update = () => {
+  getBeerList(1, 9, filters).then((list) => render(list));
+};
 
-render(filters);
+filters.init();
+update();
 
 const filtersSet = document.querySelector(".filters__set");
 
 filtersSet.addEventListener("click", () => {
-  render(filters);
+  update();
 });
 
 const filtersReset = document.querySelector(".filters__reset");
 
 filtersReset.addEventListener("click", () => {
   filters.reset();
-  render(filters);
+  update();
 });
