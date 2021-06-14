83099cb8ddd0b2e091b8fededf24e53c5fb5cdf9
igor-mircic <igor.mircic@live.com>
2021-06-14 17:17:19 +0200

    feat(cart): wip

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,8 @@
 import itemTemplate from "../templates/cart-item.handlebars";
 
-let items = [];
 const store = window.localStorage;
+let items = [];
+let total = 0;
 
 const render = (item) => {
   const cartItem = document.createElement("div");
@@ -10,20 +11,32 @@ const render = (item) => {
   $(".cart__list").append(cartItem);
 };
 
+const updateTotal = (total) => {
+  const totalPriceElement = document.querySelector(".total__price");
+  totalPriceElement.innerHTML = total;
+};
+
 export const add = (item) => {
   items.push(item);
-  console.log(items);
   store.setItem("cart", JSON.stringify(items));
+
+  total += item.abv * 10;
+  updateTotal(total);
+  store.setItem("total", total);
 
   render(item);
 };
 
 export const restore = () => {
   items = JSON.parse(store.getItem("cart"));
-  console.log(items);
   if (items) {
     items.forEach((item) => {
       render(item);
     });
+  } else {
+    items = [];
   }
+
+  total = store.getItem("total");
+  updateTotal(total);
 };
